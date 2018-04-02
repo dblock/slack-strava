9f1d769c9071c51fd56f8618d2a6a9a066b73a90
dblock <dblock@dblock.org>
2018-04-02 17:31:17 -0400

    Added maps API logs.

--- slack-strava/api/endpoints/maps_endpoint.rb
+++ slack-strava/api/endpoints/maps_endpoint.rb
@@ -9,10 +9,13 @@ module Api
           requires :id, type: String
         end
         get ':id' do
+          Api::Middleware.logger.info "Getting map ID #{params[:id]}."
           activity = Activity.where('map._id' => BSON::ObjectId(params[:id])).first
           error!('Not Found', 404) unless activity
+          Api::Middleware.logger.info "Found activity ID #{params[:id]} with URL #{activity.map.image_url}."
           content_type 'image/png'
           png = HTTParty.get(activity.map.image_url).body
+          Api::Middleware.logger.info "Returning #{png.size} byte(s) of PNG for activity ID #{params[:id]}."
           body png
         end
       end
