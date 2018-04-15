c0983afed941fbe26217f96f3cedcbbb59f79be5
dblock <dblock@dblock.org>
2018-04-15 07:50:39 -0400

    Support user_change, renaming users.

--- slack-strava/server.rb
+++ slack-strava/server.rb
@@ -28,5 +28,12 @@ module SlackStrava
         )
       end
     end
+
+    on :user_change do |client, data|
+      user = User.where(team: client.owner, user_id: data.user.id).first
+      next unless user && user.user_name != data.user.name
+      logger.info "RENAME: #{user.user_id}, #{user.user_name} => #{data.user.name}"
+      user.update_attributes!(user_name: data.user.name)
+    end
   end
 end
--- spec/slack-strava/server_spec.rb
+++ spec/slack-strava/server_spec.rb
@@ -2,10 +2,8 @@ require 'spec_helper'
 
 describe SlackStrava::Server do
   let(:team) { Fabricate(:team) }
-  let(:client) { subject.send(:client) }
-  subject do
-    SlackStrava::Server.new(team: team)
-  end
+  let(:server) { SlackStrava::Server.new(team: team) }
+  let(:client) { server.send(:client) }
   context '#channel_joined' do
     it 'sends a welcome message' do
       allow(client).to receive(:self).and_return(Hashie::Mash.new(id: 'U12345'))
@@ -36,6 +34,18 @@ describe SlackStrava::Server do
         ]
       )
       client.send(:callback, Hashie::Mash.new('user' => 'U12345', 'channel' => 'C12345'), :member_joined_channel)
+    end
+  end
+  context 'hooks' do
+    let(:user) { Fabricate(:user, team: team) }
+    it 'renames user' do
+      client.send(:callback, Hashie::Mash.new(user: { id: user.user_id, name: 'updated' }), :user_change)
+      expect(user.reload.user_name).to eq('updated')
+    end
+    it 'does not touch a user with the same name' do
+      expect(User).to receive(:where).and_return([user])
+      expect(user).to_not receive(:update_attributes!)
+      client.send(:callback, Hashie::Mash.new(user: { id: user.user_id, name: user.user_name }), :user_change)
     end
   end
 end
