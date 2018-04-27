072f54c99e25974a496b3c5998d35f82a06890ea
dblock <dblock@dblock.org>
2018-04-27 17:30:04 -0400

    Documented support for clubs.

--- public/scripts/status.js
+++ public/scripts/status.js
@@ -1,9 +1,10 @@
+
 $(document).ready(function() {
   $.ajax({
     type: "GET",
     url: "/api/status",
     success: function(data) {
-      if (data.total_distance_in_miles_s && data.connected_users_count) {
+      if (data.total_distance_in_miles_s && data.connected_users_count > 1) {
         SlackStrava.message("Together, " + data.connected_users_count + " athletes logged " + data.total_distance_in_miles_s + "!")
       }
     },
