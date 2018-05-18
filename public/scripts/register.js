b0406fc2283d754466819669d522760a6e6d4049
dblock <dblock@dblock.org>
2018-05-18 17:46:17 -0400

    Added integration test, fixed language in registration.

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         code: code
       },
       success: function(data) {
-        SlackStrava.message('Team successfully registered!<br><br>DM <b>@strava</b> or create a <b>#channel</b> and invite <b>@strava</b> to it.');
+        SlackStrava.message('Team successfully registered!<br><br>Invite <b>@slava</b> to a channel.');
       },
       error: SlackStrava.error
     });
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,5 +1,7 @@
 require 'capybara/rspec'
+
 Capybara.configure do |config|
   config.app = Api::Middleware.instance
   config.server_port = 9293
+  config.server = :webrick
 end
