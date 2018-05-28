a9b8c90ab9ad6cbfc3062c61e8b59598c23059ff
Guillaume Miossec <guillaume.miossec@gmail.com>
2018-05-28 09:29:15 +0200

    Ride activties pace is displayed in km/h if units is km

---
+++ spec/fabricators/ride_activity_fabricator.rb
@@ -0,0 +1,11 @@
+Fabricator(:ride_activity, class_name: :user_activity) do
+  strava_id { Fabricate.sequence(:user_id) { |i| "12345677892806#{i}" } }
+  type 'Ride'
+  name { Faker::Internet.user_name }
+  start_date { DateTime.parse('2018-02-20T18:02:13Z') }
+  start_date_local { DateTime.parse('2018-02-20T10:02:13Z') }
+  distance 28_099
+  moving_time 4207
+  elapsed_time 4410
+  average_speed 6.679
+end
