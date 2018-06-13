bc293248bae678dd9299b1d2888443adedae4da8
dblock <dblock@dblock.org>
2018-06-13 07:33:16 -0400

    Attempt at restarting dead teams.

--- slack-strava/app.rb
+++ slack-strava/app.rb
@@ -10,6 +10,7 @@ module SlackStrava
       end
       once_and_every 60 * 60 do
         expire_subscriptions!
+        ping_teams!
       end
       once_and_every 10 * 60 do
         brag!
@@ -28,6 +29,25 @@ module SlackStrava
       yield
       every tt do
         yield
+      end
+    end
+
+    def ping_teams!
+      Team.active.each do |team|
+        begin
+          ping = team.ping!
+          next if ping[:presence].online
+          logger.warn "DOWN: #{team}"
+          after 60 do
+            ping = team.ping!
+            unless ping[:presence].online
+              logger.info "RESTART: #{team}"
+              SlackStrava::Service.instance.start!(team)
+            end
+          end
+        rescue StandardError => e
+          logger.warn "Error pinging team #{team}, #{e.message}."
+        end
       end
     end
 
