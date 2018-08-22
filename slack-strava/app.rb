9e95c0bf4ad6dfa61d250ca27fb4d63a324cb2fa
dblock <dblock@dblock.org>
2018-08-22 08:44:58 -0400

    Upgraded dependencies, slack-ruby-bot-server 0.7.0.

--- slack-strava/app.rb
+++ slack-strava/app.rb
@@ -11,9 +11,6 @@ module SlackStrava
       once_and_every 60 * 60 do
         expire_subscriptions!
       end
-      once_and_every 60 * 3 do
-        ping_teams!
-      end
       once_and_every 10 * 60 do
         brag!
       end
@@ -31,25 +28,6 @@ module SlackStrava
       yield
       every tt do
         yield
-      end
-    end
-
-    def ping_teams!
-      Team.active.each do |team|
-        begin
-          ping = team.ping!
-          next if ping[:presence].online
-          logger.warn "DOWN: #{team}"
-          after 60 do
-            ping = team.ping!
-            unless ping[:presence].online
-              logger.info "RESTART: #{team}"
-              SlackStrava::Service.instance.start!(team)
-            end
-          end
-        rescue StandardError => e
-          logger.warn "Error pinging team #{team}, #{e.message}."
-        end
       end
     end
 
