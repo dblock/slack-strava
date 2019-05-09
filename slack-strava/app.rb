d79c83a969a9b648dc8dec19251bc824dcc76e41
dblock <dblock@dblock.org>
2019-05-09 14:53:01 -0400

    Upgraded Rubocop.

--- slack-strava/app.rb
+++ slack-strava/app.rb
@@ -41,13 +41,11 @@ module SlackStrava
     def check_trials!
       log_info_without_repeat "Checking trials for #{Team.active.trials.count} team(s)."
       Team.active.trials.each do |team|
-        begin
-          logger.info "Team #{team} has #{team.remaining_trial_days} trial days left."
-          next unless team.remaining_trial_days > 0 && team.remaining_trial_days <= 3
-          team.inform_trial!
-        rescue StandardError => e
-          logger.warn "Error checking team #{team} trial, #{e.message}."
-        end
+        logger.info "Team #{team} has #{team.remaining_trial_days} trial days left."
+        next unless team.remaining_trial_days > 0 && team.remaining_trial_days <= 3
+        team.inform_trial!
+      rescue StandardError => e
+        logger.warn "Error checking team #{team} trial, #{e.message}."
       end
     end
 
@@ -67,13 +65,11 @@ module SlackStrava
     def expire_subscriptions!
       log_info_without_repeat "Checking subscriptions for #{Team.active.count} team(s)."
       Team.active.each do |team|
-        begin
-          next unless team.subscription_expired?
-          team.subscription_expired!
-        rescue StandardError => e
-          backtrace = e.backtrace.join("\n")
-          logger.warn "Error in expire subscriptions cron for team #{team}, #{e.message}, #{backtrace}."
-        end
+        next unless team.subscription_expired?
+        team.subscription_expired!
+      rescue StandardError => e
+        backtrace = e.backtrace.join("\n")
+        logger.warn "Error in expire subscriptions cron for team #{team}, #{e.message}, #{backtrace}."
       end
     end
 
@@ -121,24 +117,22 @@ module SlackStrava
     def check_subscribed_teams!
       logger.info "Checking Stripe subscriptions for #{Team.striped.count} team(s)."
       Team.striped.each do |team|
-        begin
-          customer = Stripe::Customer.retrieve(team.stripe_customer_id)
-          customer.subscriptions.each do |subscription|
-            subscription_name = "#{subscription.plan.name} (#{ActiveSupport::NumberHelper.number_to_currency(subscription.plan.amount.to_f / 100)})"
-            logger.info "Checking #{team} subscription to #{subscription_name}, #{subscription.status}."
-            case subscription.status
-            when 'past_due'
-              logger.warn "Subscription for #{team} is #{subscription.status}, notifying."
-              team.inform_everyone!(text: "Your subscription to #{subscription_name} is past due. #{team.update_cc_text}")
-            when 'canceled', 'unpaid'
-              logger.warn "Subscription for #{team} is #{subscription.status}, downgrading."
-              team.inform_everyone!(text: "Your subscription to #{subscription.plan.name} (#{ActiveSupport::NumberHelper.number_to_currency(subscription.plan.amount.to_f / 100)}) was canceled and your team has been downgraded. Thank you for being a customer!")
-              team.update_attributes!(subscribed: false)
-            end
+        customer = Stripe::Customer.retrieve(team.stripe_customer_id)
+        customer.subscriptions.each do |subscription|
+          subscription_name = "#{subscription.plan.name} (#{ActiveSupport::NumberHelper.number_to_currency(subscription.plan.amount.to_f / 100)})"
+          logger.info "Checking #{team} subscription to #{subscription_name}, #{subscription.status}."
+          case subscription.status
+          when 'past_due'
+            logger.warn "Subscription for #{team} is #{subscription.status}, notifying."
+            team.inform_everyone!(text: "Your subscription to #{subscription_name} is past due. #{team.update_cc_text}")
+          when 'canceled', 'unpaid'
+            logger.warn "Subscription for #{team} is #{subscription.status}, downgrading."
+            team.inform_everyone!(text: "Your subscription to #{subscription.plan.name} (#{ActiveSupport::NumberHelper.number_to_currency(subscription.plan.amount.to_f / 100)}) was canceled and your team has been downgraded. Thank you for being a customer!")
+            team.update_attributes!(subscribed: false)
           end
-        rescue StandardError => e
-          logger.warn "Error checking team #{team} subscription, #{e.message}."
         end
+      rescue StandardError => e
+        logger.warn "Error checking team #{team} subscription, #{e.message}."
       end
     end
   end
