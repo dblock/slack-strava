475cff185f3f364885ea97d7377cc5969afd7d95
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2024-02-11 12:03:34 -0500

    Fix: create a new subscription after a prior canceled one.

--- spec/api/endpoints/subscriptions_endpoint_spec.rb
+++ spec/api/endpoints/subscriptions_endpoint_spec.rb
@@ -12,7 +12,7 @@ describe Api::Endpoints::SubscriptionsEndpoint do
       end
     end
     context 'subscribed team' do
-      let!(:team) { Fabricate(:team, subscribed: true, stripe_customer_id: 'customer_id') }
+      let!(:team) { Fabricate(:team, subscribed: true) }
       it 'fails to create a subscription' do
         expect {
           client.subscriptions._post(
@@ -27,19 +27,63 @@ describe Api::Endpoints::SubscriptionsEndpoint do
         end
       end
     end
-    context 'non-subscribed team with a customer_id' do
-      let!(:team) { Fabricate(:team, stripe_customer_id: 'customer_id') }
-      it 'fails to create a subscription' do
-        expect {
+    context 'team with a canceled subscription' do
+      let!(:team) { Fabricate(:team, subscribed: false, stripe_customer_id: 'customer_id') }
+      let(:stripe_customer) { double(Stripe::Customer) }
+      before do
+        allow(Stripe::Customer).to receive(:retrieve).with(team.stripe_customer_id).and_return(stripe_customer)
+      end
+      context 'with an active subscription' do
+        before do
+          allow(stripe_customer).to receive(:subscriptions).and_return([
+                                                                         double(Stripe::Subscription)
+                                                                       ])
+        end
+        it 'fails to create a subscription' do
+          expect {
+            client.subscriptions._post(
+              team_id: team.team_id,
+              stripe_token: 'token',
+              stripe_token_type: 'card',
+              stripe_email: 'foo@bar.com'
+            )
+          }.to raise_error Faraday::ClientError do |e|
+            json = JSON.parse(e.response[:body])
+            expect(json['error']).to eq 'Existing Subscription Already Active'
+          end
+        end
+      end
+      context 'without no active subscription' do
+        before do
+          allow(stripe_customer).to receive(:subscriptions).and_return([])
+        end
+        it 'updates a subscription' do
+          expect(Stripe::Customer).to receive(:update).with(
+            team.stripe_customer_id,
+            {
+              source: 'token',
+              plan: 'slava-yearly',
+              email: 'foo@bar.com',
+              metadata: {
+                id: team._id,
+                team_id: team.team_id,
+                name: team.name,
+                domain: team.domain
+              }
+            }
+          ).and_return('id' => 'customer_id')
+          expect_any_instance_of(Team).to receive(:inform!).once
+          expect_any_instance_of(Team).to receive(:inform_admin!).once
           client.subscriptions._post(
             team_id: team.team_id,
             stripe_token: 'token',
             stripe_token_type: 'card',
             stripe_email: 'foo@bar.com'
           )
-        }.to raise_error Faraday::ClientError do |e|
-          json = JSON.parse(e.response[:body])
-          expect(json['error']).to eq 'Customer Already Registered'
+          team.reload
+          expect(team.subscribed).to be true
+          expect(team.subscribed_at).to_not be nil
+          expect(team.stripe_customer_id).to eq 'customer_id'
         end
       end
     end
