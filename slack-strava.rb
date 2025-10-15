43d2c507a2dda3f646170c3525acb1bd8c81c68e
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2025-10-15 15:19:58 -0400

    Fixed Lint/RedundantDirGlobSort.

--- slack-strava.rb
+++ slack-strava.rb
@@ -3,7 +3,7 @@ ENV['RACK_ENV'] ||= 'development'
 require 'bundler/setup'
 Bundler.require :default, ENV.fetch('RACK_ENV', nil)
 
-Dir["#{File.expand_path('config/initializers', __dir__)}/**/*.rb"].sort.each do |file|
+Dir["#{File.expand_path('config/initializers', __dir__)}/**/*.rb"].each do |file|
   require file
 end
 
