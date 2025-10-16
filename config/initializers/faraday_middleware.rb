df995cd2b858119c40e38ed5055aeb7d567c94f1
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2025-10-16 10:42:57 -0400

    Upgraded strava-ruby-client to 2.3.0.

--- config/initializers/faraday_middleware.rb
+++ config/initializers/faraday_middleware.rb
@@ -1,9 +1,2 @@
 Faraday::Response::RaiseError.default_options = { include_request: true, allowed_statuses: [] }
-
-module Strava
-  module Web
-    class RaiseResponseError
-      DEFAULT_OPTIONS = { include_request: true, allowed_statuses: [] }.freeze
-    end
-  end
-end
+Strava::Web::RaiseResponseError.default_options = { include_request: true, allowed_statuses: [] }
