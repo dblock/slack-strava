e39b98373305e1c0190f2ec0d3c3d19e8b386e66
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2025-10-17 11:54:58 -0400

    Report response body for all Faraday errors.

--- slack-strava/models/brag.rb
+++ slack-strava/models/brag.rb
@@ -5,7 +5,7 @@ module Brag
     logger.warn 'Strava API rate limit exceeded.'
     NewRelic::Agent.notice_error(e, custom_params: { team: team.to_s, response_body: e.response[:body], self: to_s })
     raise e
-  rescue Strava::Errors::Fault => e
+  rescue Strava::Errors::Fault, Faraday::Error => e
     backtrace = e.backtrace.join("\n")
     logger.warn "Error in team #{team}, #{self}, #{e.message}, #{backtrace}."
     NewRelic::Agent.notice_error(e, custom_params: { team: team.to_s, response_body: e.response[:body], self: to_s })
